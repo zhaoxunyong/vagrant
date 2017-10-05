089ae44e1b62a1d0a23a0a1fb9bf27572e1547d6
zhaoxunyong <zhaoxunyong@qq.com>
2017-10-05 18:53:45 +0800

    Add files

--- boxes/ubuntu/docker.sh
+++ boxes/ubuntu/docker.sh
@@ -36,10 +36,10 @@ sudo apt-get install -y --no-install-recommends \
 
 #http://blog.csdn.net/CSDN_duomaomao/article/details/77683607
 #https://www.xtplayer.cn/2017/02/2783
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 cat >/etc/apt/sources.list.d/docker-main.list<<EOF
 deb [arch=amd64] http://mirrors.aliyun.com/docker-engine/apt/repo ubuntu-xenial main
 EOF
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 # apt-get update && apt-get upgrade -y 
 sudo apt-get update
 
